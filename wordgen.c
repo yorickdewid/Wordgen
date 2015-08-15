f0d0edd54a9ca6079dd16a989f697c405f75132b
yorickdewid <yorick17@outlook.com>
2015-08-15 14:20:29 +0200

    Use licensing

--- wordgen.c
+++ wordgen.c
@@ -1,3 +1,38 @@
+/*
+* -------------------------------  wordgen.c  -------------------------------
+*
+* Copyright (c) 2015, Yorick de Wid <yorick17 at outlook dot com>
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*   * Redistributions of source code must retain the above copyright notice,
+*     this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above copyright
+*     notice, this list of conditions and the following disclaimer in the
+*     documentation and/or other materials provided with the distribution.
+*   * Neither the name of Redis nor the names of its contributors may be used
+*     to endorse or promote products derived from this software without
+*     specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+* Generate random strings. Based on wordfield. See original code by 
+* Copyright (c) 2009 Vincent Wochnik.
+*
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,10 +43,13 @@
 
 #define DEFAULT_MAX	10
 
+static const char const prog[] = "Wordgen 1.0 - Generate random strings\nCopyright (c) 2015 Yorick de Wid";
+
 void
 usage(char *name)
 {
-	fprintf(stderr, "%s - Generate random strings\n\n", name);
+	fprintf(stderr, "%s\n", prog);
+	fprintf(stderr, "usage: %s [OPTIONS]:\n\n", name);
 	fprintf(stderr, " -v\t\tPrint additional status information\n");
 	fprintf(stderr, " -q\t\tDo not print status messages\n");
 	fprintf(stderr, " -m [MIN]\tSet minimum char count\n");
